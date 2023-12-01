7271010856d14a3a2a7dbf830208f964e155c267
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2023-12-01 08:33:47 -0500

    Day01-Part01

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string filePath = "./calibration.txt";
+string[] fileContents = File.ReadAllLines(filePath);
+
+int calibration = 0;
+
+foreach (string line in fileContents)
+{
+    calibration += GetNumber(line);
+}
+
+Console.WriteLine(calibration);
+
+
+
+
+
+static int GetNumber(string input)
+{
+    string firstDigit = string.Empty;
+    string lastDigit = string.Empty;
+    for (int index = 0; index < input.Length; index++)
+    {
+        if (string.IsNullOrEmpty(firstDigit) && char.IsDigit(input[index]))
+        {
+            firstDigit = Convert.ToString(input[index]);
+        }
+
+        int indexFromEnd = input.Length - 1 - index;
+        if (string.IsNullOrEmpty(lastDigit) && char.IsDigit(input[indexFromEnd]))
+        {
+            lastDigit = Convert.ToString(input[indexFromEnd]);
+        }
+
+        if (!string.IsNullOrEmpty(firstDigit) && !string.IsNullOrEmpty(lastDigit))
+        {
+            break;
+        }
+    }
+
+    return Convert.ToInt16($"{firstDigit}{lastDigit}");
+}
