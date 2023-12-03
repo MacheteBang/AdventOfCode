f513e4af90c50d1c87746e3d5c74174c60e69146
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2023-12-03 16:08:07 -0500

    2015-01-02

---
+++ 2015/Day01/Program.cs
@@ -0,0 +1,18 @@
+﻿string instructions = File.ReadAllText("./input.txt");
+
+int currentFloor = 0;
+int positionAtBasement = 0;
+
+for (int i = 0; i < instructions.Length; i++)
+{
+    if (instructions[i] == '(') currentFloor++;
+    if (instructions[i] == ')') currentFloor--;
+
+    if (positionAtBasement == 0 && currentFloor < 0)
+    {
+        positionAtBasement = i + 1;
+    }
+}
+
+Console.WriteLine($"Floor is:{currentFloor}");
+Console.WriteLine($"Position at Basement:{positionAtBasement}");
