bef153fb1621c9133f19386d44a08b26c92acc2c
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2023-12-06 09:46:04 -0500

    2015-04-02

--- 2015/Day04/Program.cs
+++ 2015/Day04/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+
+string secretKey = "yzbqklnj";
+string prefix = "000000";
+
+using MD5 md5 = MD5.Create();
+
+int answer = 0;
+
+for (int i = 0; i <= int.MaxValue; i++)
+{
+    string thisKey = $"{secretKey}{i}";
+    byte[] bytes = System.Text.Encoding.ASCII.GetBytes(thisKey);
+    var hash = md5.ComputeHash(bytes);
+
+    if (Convert.ToHexString(hash).StartsWith(prefix))
+    {
+        answer = i;
+        break;
+    }
+}
+
+
+Console.WriteLine($"Number is {answer}");
+
+Console.WriteLine("End of Program");
