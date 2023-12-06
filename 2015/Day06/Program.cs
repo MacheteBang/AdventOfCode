6de1cdbed99b03034e68bdddafd61f0a3bd7dfb2
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2023-12-06 11:52:23 -0500

    2015-06-01

--- 2015/Day06/Program.cs
+++ 2015/Day06/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string inputFile = "./input.txt";
+string[] lines = File.ReadAllLines(inputFile);
+
+bool[,] lightArray = new bool[1000, 1000];
+
+foreach (string line in lines)
+{
+    string[] sides = line.Split(" through ");
+    Coordinate end = new() { X = int.Parse(sides[1].Split(',')[0]), Y = int.Parse(sides[1].Split(',')[1]) };
+
+    string action = string.Empty;
+    action = sides[0].Contains("on") ? "On" : (sides[0].Contains("off") ? "Off" : "Toggle");
+    Coordinate start = new() { X = int.Parse(sides[0].Split(" ").Last().Split(',')[0]), Y = int.Parse(sides[0].Split(" ").Last().Split(',')[1]) };
+
+    UpdateGrid(ref lightArray, action, start, end);
+}
+
+int countOfLightsOn = lightArray.Cast<bool>().Count(l => l);
+
+Console.WriteLine($"Lights On: {countOfLightsOn}");
+Console.WriteLine("End of Program");
+
+
+void UpdateGrid(ref bool[,] grid, string action, Coordinate start, Coordinate end)
+{
+    for (int x = start.X; x <= end.X; x++)
+    {
+        for (int y = start.Y; y <= end.Y; y++)
+        {
+            grid[x, y] = action == "On" ? true : (action == "Off" ? false : !grid[x, y]);
+        }
+    }
+
+}
